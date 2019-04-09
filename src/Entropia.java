12a4523bff7eb3ac85d8b44636096b6cf63d497c
Luis Felipe Landa Lizarralde <flandalanda@gmail.com>
2019-04-09 18:53:26 -0500

    Working version

--- src/Entropia.java
+++ src/Entropia.java
@@ -15,6 +15,7 @@ import java.io.RandomAccessFile;
 import java.io.Writer;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,48 +28,72 @@ public class Entropia {
     public static void main(String[] args) throws IOException{
         try {
             //String path = System.getProperty("user.dir");
-            RandomAccessFile arch = new RandomAccessFile("capPNG.png", "r");
+            RandomAccessFile arch; //= new RandomAccessFile("capPNG.png", "r");
             int [] frec = new int [256];
             double [] prob = new double [256];
-            double len = arch.length();
-            double cont = 0;
-            double res = 0;
+            double len; //= arch.length();
+            double cont;
+            double res;
             int pos;
             
             for (int i = 0; i < 256; i++) {
                 frec[i] = 0;
             }
             
-            try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-              new FileOutputStream("res.csv"), "utf-8"))) {
+            Scanner lee = new Scanner(System.in);
+            String lec = "";
+            
+            while(!lec.equalsIgnoreCase("N")){
                 
-                while(cont<len){
-                    pos = arch.read();
-                    frec[pos] = frec[pos]+1;
-                    cont++;
-                }
+                System.out.println("Escriba el nombre del archivo a analizar");
                 
+                lec = lee.nextLine();
+                
+                arch = new RandomAccessFile(lec, "r");
+                len = arch.length();
+                
+                System.out.println("Escriba el nombre del archivo donde se guardarán los resultados");
+                
+                lec = lee.nextLine();
                 
                 for (int i = 0; i < 256; i++) {
-                    prob[i] = frec[i]/len;                
-                    res += prob[i]*Math.log(prob[i])/Math.log(2);
-                    
-                    writer.write("Probabiliad de valor " + i + ": "+ prob[i]+", ");
+                    frec[i] = 0;
+                    prob[i] = 0;
                 }
                 
-                res = -1*res;
+                cont = 0;
+                res = 0;
                 
-                System.out.println(len);
+                try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(lec), "utf-8"))) {
                 
-                for (double i : prob) {
-                    System.out.println(i);
+                    while(cont<len){
+                        pos = arch.read();
+                        frec[pos] = frec[pos]+1;
+                        cont++;
+                    }
+                
+                
+                    for (int i = 0; i < 256; i++) {
+                        prob[i] = frec[i]/len;                
+                        res += prob[i]*Math.log(prob[i])/Math.log(2);
+                    
+                        writer.write("Probabiliad del símbolo " + i + ": "+ prob[i]+ System.lineSeparator());
+                    }
+                
+                    res = -1*res;
+                
+                    writer.write("El valor de la entropía en el archivo es de: " + res);
+                
+                    System.out.println("Desea analizar otro archivo?(Y/N)");
+                    
+                    lec = lee.nextLine();
                 }
             }
-            
         } catch (FileNotFoundException ex) {
             System.out.println("ERROR");
             Logger.getLogger(Entropia.class.getName()).log(Level.SEVERE, null, ex);
         }
+                
     }
-    
 }
